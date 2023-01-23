5d94f0d4e4bbb54276846a9672d7a8c41fd8ce15
José Rodolpho <j.rodolpho29@hotmail.com>
2023-01-23 05:07:12 -0300

    feat: auth tests

---
+++ prisma/migrations/20230123080519_create_test_database/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `final` on the `Robot` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Robot" DROP COLUMN "final";
