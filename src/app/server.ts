a6e6c7b243267ba3cc7711bf1af3822d9837444c
José Rodolpho <j.rodolpho29@hotmail.com>
2023-01-23 14:49:51 -0300

    feat: config for deploy

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,7 +2,8 @@ import app from "./index.js"
 import dotenv from "dotenv"
 dotenv.config()
 
+const host = "0.0.0.0"
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+app.listen(PORT, host, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
